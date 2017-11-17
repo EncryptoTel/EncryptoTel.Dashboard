6e4f7aac5496861a1bd3b90af31df957465724ca
Wondergrauf <wondergrauf@gmail.com>
2017-11-17 15:07:34 +0300

    Work in progress. Details: + Minor fixes

--- src/shared/vars.ts
+++ src/shared/vars.ts
@@ -5,7 +5,7 @@ import {environment} from '../environments/environment';
  */
 
 export const back = environment.production ?
-  'http://pbx.encryptotel.com/api' : environment.title === 'test' ?
+  'http://pbx.encryptotel.com/api' : environment.title === 'local' ?
     'http://pbx-front-test.dev/api' : 'http://pbx-front-dev.encry.ru/api';
 
 /*
