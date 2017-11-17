1cd6020d375bdf7c11d1db93e511af8af45a996f
Wondergrauf <wondergrauf@gmail.com>
2017-11-17 12:40:14 +0300

    Work in progress. Details: + Minor fixes

--- src/components/index/index.component.ts
+++ src/components/index/index.component.ts
@@ -1,10 +1,19 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {MessageServices} from '../../services/message.services';
 
 @Component({
   selector: 'project-index',
-  templateUrl: './template.html'
+  templateUrl: './template.html',
+  styles: [`button{width: 100px;margin: auto}`]
 })
 
 export class IndexComponent {
-  constructor() {}
+  constructor(private router: Router,
+              private messages: MessageServices) {}
+  logout() {
+    localStorage.clear();
+    this.messages.writeSuccess('Logout successful');
+    return this.router.navigate(['../sign-in']);
+  }
 }
