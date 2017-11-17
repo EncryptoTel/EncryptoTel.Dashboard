59afbbe79fadc8a34f589ab2e92d1647de9865ba
Wondergrauf <wondergrauf@gmail.com>
2017-11-17 12:22:11 +0300

    Work in progress. Details: + Minor fixes

--- src/components/index/index.component.ts
+++ src/components/index/index.component.ts
@@ -1,14 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'project-index',
   templateUrl: './template.html'
 })
 
-export class IndexComponent implements OnInit {
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+export class IndexComponent {
+  constructor() {}
 }
