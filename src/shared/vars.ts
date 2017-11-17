fb7d2596bfa9840e3f46437bb62ba71b17fa4f61
Wondergrauf <wondergrauf@gmail.com>
2017-11-17 11:47:50 +0300

    Work in progress. Details: + Minor fixes

--- src/shared/vars.ts
+++ src/shared/vars.ts
@@ -8,6 +8,10 @@ export const back = environment.production ?
   'http://pbx.encryptotel.com/api' : environment.title === 'test' ?
     'http://pbx-front-test.dev/api' : 'http://pbx-front-dev.encry.ru/api';
 
+/*
+  Regular expressions
+ */
+
 export const emailRegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
 export const nameRegExp = new RegExp(/^[\a-z]([\a-z-.]*)+$/i);
