9b358b6bf4b905bdd187c162f933394a3f8dadbe
Anton Ryzanov <a.ryazanov@encry.ru>
2018-04-05 17:45:47 +0300

    fix auth route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   title: 'development',
   production: false,
   back: 'http://pbx-back-dev.encry.ru/api',
-  auth: 'http://pbx-back-dev.encry.ru/auth',
+  auth: 'http://pbx-back-dev.encry.ru/api',
   ph: 'assets/json',
   params: {
     messageDuration: 5000
