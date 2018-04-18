f12d0ea687ec3b74130244e23a2f814ec815aacc
Wondergrauf <wondergrauf@gmail.com>
2018-04-18 12:53:18 +0300

    Work in progress.   Details:     + Added multiple selection option for table     + Fixed table buttons events propogation

--- src/elements/pbx-table/pbx-table.component.ts
+++ src/elements/pbx-table/pbx-table.component.ts
@@ -9,19 +9,33 @@ import {TableInfoModel} from '../../models/table-info.model';
 
 export class TableComponent {
   @Input() tableItems: any[];
-  @Input() selected: object;
+  @Input() selected: any;
   @Input() tableInfo: TableInfoModel;
   @Input() editable: boolean;
+  @Input() multiple: boolean;
   @Output() onSelect: EventEmitter<object> = new EventEmitter<object>();
   @Output() onEdit: EventEmitter<object> = new EventEmitter<object>();
   @Output() onDelete: EventEmitter<number> = new EventEmitter<number>();
+  isSelected(id: number): boolean {
+    if (this.selected) {
+      return !!this.selected.find(item => item.id === id);
+    }
+  }
   selectItem(item): void {
     this.onSelect.emit(item);
   }
-  editItem(item): void {
+  editItem(item, ev: MouseEvent): void {
+    if (ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
+    }
     this.onEdit.emit(item);
   }
-  deleteItem(id): void {
+  deleteItem(id, ev: MouseEvent): void {
+    if (ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
+    }
     this.onDelete.emit(id);
   }
 }
