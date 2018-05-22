22f2782918f3ce6c889b1a6e13184ebe40f439a8
Wondergrauf <wondergrauf@gmail.com>
2018-05-22 18:37:41 +0300

    Work in progress.   Details:     + Added tariff plans backend request     + Added marketplace backend request     + Added backend request for sign up tariffs list     + Improved table element (now it may accept nested properties)     + Other minor fixes and improvements

--- src/components/marketplace/marketplace.component.ts
+++ src/components/marketplace/marketplace.component.ts
@@ -1,33 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Module} from '../../models/module.model';
+import {ModuleServices} from '../../services/module.services';
 
 @Component({
   selector: 'pbx-marketplace',
   templateUrl: './template.html',
-  styleUrls: ['./local.sass']
+  styleUrls: ['./local.sass'],
+  providers: [ModuleServices]
 })
 
-export class MarketplaceComponent {
-  modules = [
-    new Module(1, 'Module COMPANY', 'Company information', 0, true, 1),
-new Module(2, 'Module IVR', 'Interactive voice response (IVR) is a technology that allows a computer to interact with humans through the use of voice and DTMF tones input via keypad.', 10, false, 2),
-    new Module(3, 'Module Call-center', '', 10, false, 2),
-    new Module(4, 'Module Queue', '', 10, false, 2),
-    new Module(5, 'Module SIP TRAFFIC ENCRYPTION', '', 10, false, 3),
-    new Module(6, 'Module Ext 50', 'Amount of Ext numbers 50', 0, false, 4),
-    new Module(7, 'Module Ext 100', 'Amount of Ext numbers 100', 10, false, 4),
-    new Module(8, 'Module Schedule', '', 0, false, 5),
-    new Module(9, 'Module Storage 500', 'Storage space 500 mb', 10, false, 6),
-    new Module(10, 'Module Storage 1000', 'Storage space 1000 mb', 10, false, 6),
-    new Module(11, 'Module Storage 1500', 'Storage space 1500 mb', 10, false, 6),
-  ];
+export class MarketplaceComponent implements OnInit {
+  modules: Module[];
+
+  constructor(private _services: ModuleServices) {}
+
+  ngOnInit(): void {
+    this.modules = [];
+    this._services.getModulesList()
+      .then(res => {
+        res.map(module => {
+          this.modules.push({
+            id: module.service.id,
+            title: module.service.title,
+            content: module.service.description,
+            price: module.currentPrice.summWithVat,
+            status: false, // TODO: required status in backend response
+            color: Math.round(Math.random() * 5 + 1) // TODO: required color in backend response
+          });
+        });
+      }).catch();
+  }
 }
 
-export class Module {
-  constructor (
-    public id: number,
-    public title: string,
-    public content: string,
-    public cost: number,
-    public status: boolean,
-    public color: number) {}
-}
+
