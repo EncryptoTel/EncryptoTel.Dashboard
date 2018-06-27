5285223d040a9bab2d7344743dfb28aca82f0ed0
gapaus <a.sabirzyanov@encryptotel.com>
2018-06-28 00:35:22 +0300

    tariff plan sum

--- src/components/tariff-plans/tariff-plans.component.ts
+++ src/components/tariff-plans/tariff-plans.component.ts
@@ -7,99 +7,105 @@ import {StorageServices} from "../../services/storage.services";
 import {UserServices} from "../../services/user.services";
 
 @Component({
-  selector: 'pbx-tariff-plans',
-  templateUrl: './template.html',
-  styleUrls: ['./local.sass'],
-  animations: [SwipeAnimation('y', '300ms'), FadeAnimation('300ms')],
-  providers: [TariffPlanServices]
+    selector: 'pbx-tariff-plans',
+    templateUrl: './template.html',
+    styleUrls: ['./local.sass'],
+    animations: [SwipeAnimation('y', '300ms'), FadeAnimation('300ms')],
+    providers: [TariffPlanServices]
 })
 
 export class TariffPlansComponent implements OnInit {
-  loading = true;
+    loading = true;
 
-  tariffs = [];
-  // currentTariff = 2;
-  currentPick = -1;
-  page = 1;
-  selected: any;
+    tariffs = [];
+    // currentTariff = 2;
+    currentPick = -1;
+    page = 1;
+    selected: any;
 
-  modal: {
-    visible: boolean,
-    title: string,
-    confirm: {type: string, value: string},
-    decline: {type: string, value: string}
-  };
+    modal: {
+        visible: boolean,
+        title: string,
+        confirm: { type: string, value: string },
+        decline: { type: string, value: string }
+    };
 
-  constructor(private _service: TariffPlanServices,
-              private _storage: StorageServices,
-              private _user: UserServices) {
-      this.modal = {
-          visible: false,
-          title: '',
-          confirm: {type: 'success', value: 'Yes'},
-          decline: {type: 'error', value: 'No'}
-      };
-  }
+    constructor(private _service: TariffPlanServices,
+                private _storage: StorageServices,
+                private _user: UserServices) {
+        this.modal = {
+            visible: false,
+            title: '',
+            confirm: {type: 'success', value: 'Yes'},
+            decline: {type: 'error', value: 'No'}
+        };
+    }
 
-  PageCount() {
-    return Math.round(this.tariffs.length / 4) + (this.tariffs.length % 4 === 1 ? 1 : 0);
-  }
+    PageCount() {
+        return Math.round(this.tariffs.length / 4) + (this.tariffs.length % 4 === 1 ? 1 : 0);
+    }
 
-  getCurrentTariff(): any {
-    const user = this._storage.readItem('pbx_user');
-    return user['profile']['tariffPlan'];
-  }
+    getCurrentTariff(): any {
+        const user = this._storage.readItem('pbx_user');
+        return user['profile']['tariffPlan'];
+    }
 
-  isCurrentTariff(tariff: any): boolean {
-    return this.getCurrentTariff().id === tariff.id;
-  }
+    isCurrentTariff(tariff: any): boolean {
+        return this.getCurrentTariff().id === tariff.id;
+    }
 
-  chooseTariff(tariff: any): void {
-    this.selected = tariff;
-    this.modal.visible = true;
-  }
+    chooseTariff(tariff: any): void {
+        this.selected = tariff;
+        this.modal.visible = true;
+    }
 
-  tariffCost(tariff: any): string {
-    return tariff.price > 0 ? 'From $' + tariff.price +'/monthly' : 'FREE';
-  }
+    tariffCost(tariff: any): string {
+        return tariff.price > 0 ? 'From $' + tariff.price + '/monthly' : 'FREE';
+    }
 
-  tariffStatus(tariff: any): string {
-    return this.getCurrentTariff().id === tariff.id ? 'Subscribed' : (tariff.price > 0 ? 'Buy now' : 'Free');
-  }
+    tariffStatus(tariff: any): string {
+        return this.getCurrentTariff().id === tariff.id ? 'Subscribed' : (tariff.price > 0 ? 'Buy now' : 'Free');
+    }
 
-  modalConfirm = (): void => {
-    this.selected.loading = true;
-    this._service.selectTariffPlan(this.selected.id)
-        .then(res => {
-            this._user.fetchProfileParams()
-                .then(res => {
-                    this.selected.loading = false;
-                });
-        }).catch();
+    modalConfirm = (): void => {
+        this.selected.loading = true;
+        this._service.selectTariffPlan(this.selected.id)
+            .then(res => {
+                this._user.fetchProfileParams()
+                    .then(res => {
+                        this.selected.loading = false;
+                    });
+            }).catch();
 //    console.log('Modal confirmed!');
-  }
+    }
 
-  modalDecline = (): void => {
-    // console.log('Modal declined!');
-  }
+    modalDecline = (): void => {
+        // console.log('Modal declined!');
+    }
 
-  ngOnInit(): void {
-    this._service.getTariffPlans().then(res => {
-      res.map(tariff => {
-        this.tariffs.push({
-          id: tariff.id,
-          title: tariff.title,
-          price: tariff.sum,
-          services: []
-        });
-        tariff.offers.map(offer => {
-          this.tariffs[this.tariffs.length - 1].services.push({
-            title: offer.service.title
-          });
-        });
-      });
-      this.loading = false;
-    }).catch();
-  }
+    ngOnInit(): void {
+        this._service.getTariffPlans().then(res => {
+            res.map(tariff => {
+                let price = 0;
+                tariff.offers.map(offer => {
+                    price += offer.service.sumWithVat;
+                    // console.log(offer.service);
+                });
+                price = Math.round(price * 100) / 100;
+                this.tariffs.push({
+                    id: tariff.id,
+                    title: tariff.title,
+                    price: price,
+                    services: []
+                });
+                tariff.offers.map(offer => {
+                    this.tariffs[this.tariffs.length - 1].services.push({
+                        title: offer.service.title
+                    });
+                });
+            });
+            this.loading = false;
+        }).catch();
+    }
 
 }
