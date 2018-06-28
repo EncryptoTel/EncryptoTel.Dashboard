cbdcbceb9aa69cc59909f21876d259869ce8a317
xkubic <xkubic@yandex.ru>
2018-06-28 14:29:25 +0300

    work in progress   + 'buy-number' template

--- src/components/phone-numbers/buy/buy.phone-numbers.component.ts
+++ src/components/phone-numbers/buy/buy.phone-numbers.component.ts
@@ -1,9 +1,9 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {PhoneNumbersServices} from '../../../services/phone-numbers.services';
 import {calculateHeight} from '../../../shared/shared.functions';
-import {templateJitUrl} from "@angular/compiler";
-import {CountryModel} from "../../../models/country.model";
-import {CountryServices} from "../../../services/country.services";
+import {templateJitUrl} from '@angular/compiler';
+import {CountryModel} from '../../../models/country.model';
+import {CountryServices} from '../../../services/country.services';
 
 @Component({
     selector: 'buy-phone-numbers-component',
@@ -65,7 +65,7 @@ export class BuyPhoneNumbersComponent implements OnInit {
                 // this.list = [res['items'].slice(0, this.requestDetails.limit / 2), res['items'].slice(this.requestDetails.limit / 2)];
                 this.list = [res['numbers'].slice(0, this.requestDetails.limit / 2), res['numbers'].slice(this.requestDetails.limit / 2)];
                 // console.log(this.list);
-                this.pagination.total = 1;//res.pages;
+                this.pagination.total = 1; // res.pages;
                 this.loading = false;
             });
     }
@@ -125,7 +125,7 @@ export class BuyPhoneNumbersComponent implements OnInit {
             countryCode: 'US',
             search: '',
             page: 1,
-            //limit: calculateHeight(this.table, this.row) * 2
+            // limit: calculateHeight(this.table, this.row) * 2
             limit: 40
         };
         this.getList();
