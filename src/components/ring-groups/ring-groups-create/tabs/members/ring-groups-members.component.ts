911897c1e07649af6277beba30294634555468cd
gapaus <a.sabirzyanov@encryptotel.com>
2018-07-03 14:20:54 +0300

    fix merge

---
+++ src/components/ring-groups/ring-groups-create/tabs/members/ring-groups-members.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {CallQueuesServices} from '../../../../../services/call-queues.services';
+import {SipInner} from '../../../../../models/queue.model';
+import {FadeAnimation} from '../../../../../shared/fade-animation';
+
+@Component({
+  selector: 'ring-groups-members',
+  templateUrl: './template.html',
+  styleUrls: ['./local.sass'],
+  animations: [FadeAnimation('300ms')]
+})
+
+export class RingGroupsMembersComponent {
+  constructor(private _services: CallQueuesServices) {
+    this._services.userView.isCurCompMembersAdd = false;
+  }
+
+  members: SipInner[] = this._services.userView.members;
+
+  table = {
+    title: {
+      titles: ['#Ext', 'Phone number', 'First Name', 'Last Name', 'Status'],
+      keys: ['phoneNumber', 'sipOuterPhone', 'firstName', 'lastName', 'statusName']
+    }
+  };
+
+  deleteMember(memberId) {
+    const indexCallQueue = this._services.callQueue.queueMembers.findIndex(el => {
+      if (el.sipId === memberId) {
+        return true;
+      }
+    });
+    const indexView = this._services.userView.members.findIndex(el => {
+      if (el.id === memberId) {
+        return true;
+      }
+    });
+    this._services.callQueue.queueMembers.splice(indexCallQueue, 1);
+    this._services.userView.members.splice(indexView, 1);
+  }
+}
--- src/elements/pbx-notificator/pbx-notificator.component.ts
+++ src/elements/pbx-notificator/pbx-notificator.component.ts
@@ -1,108 +1,137 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, HostListener, Input, OnChanges, OnDestroy, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    DoCheck,
+    HostListener,
+    Input,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    SimpleChange,
+    SimpleChanges
+} from '@angular/core';
 import {SwipeAnimation} from '../../shared/swipe-animation';
 
 @Component({
-  selector: 'pbx-notificator',
-  templateUrl: './template.html',
-  styleUrls: ['./local.sass'],
-  animations: [SwipeAnimation('y','400ms')]
+    selector: 'pbx-notificator',
+    templateUrl: './template.html',
+    styleUrls: ['./local.sass'],
+    animations: [SwipeAnimation('y', '400ms')]
 })
 
 export class NotificatorComponent implements OnInit, OnChanges {
-  @Input() notificator: {
-    timer: any,
-    visible: boolean,
-    type: string,
-    message: string,
-    actionType: string,
-    actionName: string
-  };
+    @Input() notificator: {
+        timer: any,
+        visible: boolean,
+        type: string,
+        message: string,
+        actionType: string,
+        actionName: string
+    };
 
-  timer: any;
+    timer: any;
 
-  queue = [];
+    queue = [];
+    qu = [];
+    test = 0;
+    notificatorWidth: number;
 
-  notificatorWidth: number;
-
-  constructor() {
-    this.notificatorWidth = (window.innerWidth - 616);
-  }
-
-  ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    // console.log(changes.notificator.currentValue.visible);
-    // console.log(changes);
-    // const change = changes;
-    //
-    // if (typeof change.notificator.currentValue !== 'undefined') {
-    //   this.queue.push(change.notificator.currentValue);
-    //   console.log(this.queue);
-    //
-    //   change.notificator.currentValue.visible = false;
-    //
-    //   if (this.queue.length === 1) {
-    //     // change.notificator.currentValue.visible = true;
-    //     // this.queue[0].visible = true;
-    //     change.notificator.currentValue.timer = setTimeout(this.timerF, 2000);
-    //   }
-    //
-    // }
-
-    this.notificator = changes.notificator.currentValue;
-
-    if (this.notificator) {
-      const timout = setTimeout(() => {
-        this.notificator.visible = false;
-      }, 4000);
+    constructor() {
+        this.notificatorWidth = (window.innerWidth - 616);
     }
-  }
 
-  // timerF() {
-  //   if (typeof this.queue !== 'undefined') {
-  //     const shift = this.queue.shift();
-  //     // this.queue[0].notificator.visible = false;
-  //     if (this.queue[0]) {
-  //       this.queue[0].notificator.visible = true;
-  //       this.queue[0].notificator.timer = setTimeout(this.timerF, 2000);
-  //     }
-  //     alert(shift.notificator.currentValue);
-  //   }
-  // }
+    ngOnInit() {
+        // this.example();
+    }
 
-  // example() {
-  //   const qu = [];
-  //
-  //   function func() {
-  //     const s = qu.shift();
-  //     if (qu.length > 0) {
-  //       setTimeout(func, 1000);
-  //     }
-  //     alert( s );
-  //   }
-  //
-  //   function start() {
-  //     qu.push('Hello!');
-  //     qu.push('World!');
-  //     qu.push('Vasya!');
-  //     qu.push('Kisa!');
-  //     setTimeout(func, 1000);
-  //   }
-  //
-  //   start();
-  // }
+    ngOnChanges(changes: SimpleChanges) {
+        // console.log(changes.notificator.currentValue.visible);
+        // console.log(changes);
+        // const change = changes;
+        //
+        // if (typeof change.notificator.currentValue !== 'undefined') {
+        //   this.queue.push(change.notificator.currentValue);
+        //   console.log(this.queue);
+        //
+        //   change.notificator.currentValue.visible = false;
+        //
+        //   if (this.queue.length === 1) {
+        //     // change.notificator.currentValue.visible = true;
+        //     // this.queue[0].visible = true;
+        //     change.notificator.currentValue.timer = setTimeout(this.timerF, 2000);
+        //   }
+        //
+        // }
+        // this.test++;
+        // this.qu.push(this.test);
+        // setTimeout(this.func, 5000);
+        // this.notificator = changes.notificator.currentValue;
 
-  notificatorAction() {
-    this.notificator.visible = false;
-    clearTimeout(this.timer);
-  }
+        // if (this.notificator) {
+        //   const timout = setTimeout(() => {
+        //     this.notificator.visible = false;
+        //   }, 4000);
+        // }
+        this.example(changes);
+    }
 
-  @HostListener('window:resize', ['$event'])
+    timerF() {
+        if (typeof this.queue !== 'undefined') {
+            const shift = this.queue.shift();
+            // this.queue[0].notificator.visible = false;
+            if (this.queue[0] && this.queue[0].notificator) {
+                this.queue[0].notificator.visible = true;
+                this.queue[0].notificator.timer = setTimeout(this.timerF, 2000);
+            }
 
-  onResize(event) {
-    console.log('Width: ' + event.target.innerWidth);
-    this.notificatorWidth = (window.innerWidth - 616);
-  }
+        }
+    }
+
+    func() {
+        const s = this.qu.shift();
+        alert(s);
+        if (this.qu.length > 0) {
+            setTimeout(this.func, 5000);
+        }
+    }
+
+    example(changes: SimpleChanges) {
+        let _this = this;
+
+        function func() {
+            if (typeof _this.qu !== 'undefined') {
+                const shift = _this.qu.shift();
+                if (shift) {
+                    shift.visible = false;
+                }
+                if (_this.qu[0]) {
+                    _this.qu[0].visible = true;
+                    _this.qu[0].timer = setTimeout(this.timerF, 2000);
+                }
+            }
+        }
+
+        function start(ch: SimpleChanges) {
+            _this.qu.push(ch.notificator.currentValue);
+            setTimeout(func, 5000);
+        }
+
+        start(changes);
+    }
+
+    notificatorAction() {
+        if (this.notificator) {
+            this.notificator.visible = false;
+        }
+        clearTimeout(this.timer);
+    }
+
+    @HostListener('window:resize', ['$event'])
+
+    onResize(event) {
+        console.log('Width: ' + event.target.innerWidth);
+        this.notificatorWidth = (window.innerWidth - 616);
+    }
 
 }
