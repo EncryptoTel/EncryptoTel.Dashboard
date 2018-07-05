dfa7684dcaf8b9a621a2eaa8ee5a54780610ce52
xkubic <xkubic@yandex.ru>
2018-07-05 13:03:40 +0300

    work in progress   storage     template, css

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,17 +1,32 @@
 import {BaseService} from "./base.service";
+import {RequestServices} from './request.services';
+import {Injectable} from '@angular/core';
 
-export class StorageService extends BaseService {
+@Injectable()
 
-    uploadFile(data: FormData): Promise<any> {
-        return this.rawRequest('POST', '', data);
-    }
+export class StorageService  {
+  constructor(
+    private _req: RequestServices
+  ) {}
 
-    getList(): Promise<any> {
-        return this.get('');
-    }
+  /*uploadFile(data: FormData): Promise<any> {
+      return this.rawRequest('POST', '', data);
+  }*/
 
-    onInit() {
-        this.url = 'v1/account/file';
-    }
+  getList(pageInfo: any, type: string): Promise<any> {
+    return this._req.get(`v1/account/file?limit=${pageInfo.limit}&page=${pageInfo.page}&type=${type}`, true);
+  }
+
+  getTypes(): Promise<any> {
+    return this._req.get('v1/handbooks/account/file/get-types', true);
+  }
+
+  /*getList(): Promise<any> {
+      return this.get('');
+  }*/
+
+  /*onInit() {
+      this.url = 'v1/account/file';
+  }*/
 
 }
