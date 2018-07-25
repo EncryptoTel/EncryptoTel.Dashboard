1345470ec3afd67e5d44c0c0c04e0641507bf507
s.kozulyaev <s.kozulyaev@encry.ru>
2018-07-25 15:15:16 +0300

    fix Knowledge Base

--- src/components/knowledge-base/knowledge-base.component.ts
+++ src/components/knowledge-base/knowledge-base.component.ts
@@ -1,23 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {PartnerProgramService} from '../../services/partner-program.service';
 import {PartnerProgramModel} from '../../models/partner-program.model';
 import {SwipeAnimation} from '../../shared/swipe-animation';
 import {PageInfoModel} from '../../models/base.model';
+import {ButtonItem, FilterItem, HeaderComponent} from '../../elements/pbx-header/pbx-header.component';
 
 @Component({
     selector: 'partner-program-component',
     templateUrl: './template.html',
-    styleUrls: ['./local.sass'],
+    styleUrls: ['./local.less'],
     providers: [PartnerProgramService],
     animations: [SwipeAnimation('x', '300ms')]
 })
 
 export class KnowledgeBaseComponent implements OnInit {
+    @Input() buttons: ButtonItem[] = [];
+    filters: FilterItem[] = [];
 
     constructor(private service: PartnerProgramService) {
-
+      const filterValue = [
+        {id: 'all', title: 'All'},
+        {id: 'account', title: 'Account'},
+      ];
+      this.filters.push(new FilterItem(1, 'type', 'Select Source', filterValue, 'title'));
+      this.filters.push(new FilterItem(2, 'search', 'Search', null, null, 'Search Pbx support'));
     }
 
+    reloadFilter(filter) {
+      // this.filters = filter;
+    }
+
+    updateFilter(filter) {
+      this.filters = filter;
+    }
 
     ngOnInit(): void {
 
