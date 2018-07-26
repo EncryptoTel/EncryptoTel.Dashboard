cddd6197876107406b554fe104a30f80b2824083
s.kozulyaev <s.kozulyaev@encry.ru>
2018-07-26 21:05:38 +0300

    fix font

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
 export const environment = {
     title: 'development',
     production: false,
-    back: 'http://pbx-back-dev.encry.ru/api',
-    // back: 'http://pbx.loc/api',
-    ws: 'ws.pbx-back-dev.encry.ru',
-    // ws: 'localhost:3000',
+    back: 'http://back-symfony.loc/api',
+    ws: 'localhost:3000',
 };
