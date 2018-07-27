bc95478b5db2d58a18679286427a514a3732bb9e
s.kozulyaev <s.kozulyaev@encry.ru>
2018-07-27 19:49:38 +0300

    fix style

--- src/components/settings/settings-items/profile/profile.component.ts
+++ src/components/settings/settings-items/profile/profile.component.ts
@@ -139,7 +139,7 @@ export class ProfileComponent implements OnInit {
             if (this.generalForm.valid) {
                 this.service.saveProfileSettings(this.generalForm.value)
                     .then((res) => {
-                        // this.getSettings();
+                        this.getSettings();
                         this.user.fetchProfileParams().then();
                         //this.goBack();
                     })
