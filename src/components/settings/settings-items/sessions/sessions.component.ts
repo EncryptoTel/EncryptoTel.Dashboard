da13be26a7cefdc825fd6eaf6935fc6a6af7ec68
gapaus <a.sabirzyanov@encryptotel.com>
2018-08-02 19:55:03 +0300

    user sessions

--- src/components/settings/settings-items/sessions/sessions.component.ts
+++ src/components/settings/settings-items/sessions/sessions.component.ts
@@ -4,6 +4,7 @@ import {OnInit} from "@angular/core";
 import {SessionsModel} from "../../../../models/settings.models";
 import {ButtonItem, TableInfoExModel, TableInfoItem} from "../../../../models/base.model";
 import {SessionsService} from "../../../../services/sessions.service";
+import {Router} from "@angular/router";
 
 @AnimationComponent({
     selector: 'profile-component',
@@ -18,14 +19,19 @@ export class SessionsComponent extends BaseComponent implements OnInit {
     table: TableInfoExModel = new TableInfoExModel();
     buttons: ButtonItem[] = [];
 
-    constructor(public service: SessionsService) {
+    constructor(public service: SessionsService,
+                private router: Router) {
         super();
         this.table.items.push(new TableInfoItem('User Token', 'userToken', null, 300));
         this.table.items.push(new TableInfoItem('IP', 'ip'));
         this.table.items.push(new TableInfoItem('Country', 'country'));
         this.table.items.push(new TableInfoItem('Expires At', 'displayExpires'));
         // this.table.items.push(new TableInfoItem('User Agent', 'userAgent'));
-        this.buttons.push(new ButtonItem(0, '', '', false));
+        this.buttons.push(new ButtonItem(0, 'Back', 'cancel', true));
+    }
+
+    back() {
+        this.router.navigate(['cabinet', 'settings']);
     }
 
     ngOnInit() {
