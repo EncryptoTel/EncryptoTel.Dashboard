1db2f900b163ad845f0823b9380ea7d8054468a3
s.kozulyaev <s.kozulyaev@encry.ru>
2018-08-10 18:08:15 +0300

    some fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     title: 'development',
     production: false,
-    back: 'http://back-symfony.loc/api',
-    ws: 'localhost:3000'
+    back: 'http://pbx-back-dev.encry.ru/api',
+    ws: 'ws.pbx-back-dev.encry.ru'
 };
