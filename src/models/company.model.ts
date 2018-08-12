c5d96e674f411be3f1d38af8b0393978c46707b0
s.lebedinskiy <s.lebedinskiy@encry.ru>
2018-08-12 13:21:14 +0300

    class CompanyInfoEvaluator added

--- src/models/company.model.ts
+++ src/models/company.model.ts
@@ -1,5 +1,6 @@
 import {CountryModel} from "./country.model";
-import { DatePipe } from '@angular/common';
+import {DatePipe, DecimalPipe} from '@angular/common';
+import { format } from "util";
 
 export class CompanyModel {
     constructor(public name: string,
@@ -37,7 +38,8 @@ export class CompanyInfoModel {
     public setCompanyData(company: CompanyModel): void {
         this.company.forEach(item => {
             if (item.key) {
-                item.value = this.eval(item.key, company, item.type);
+                let evaluator = new CompanyInfoEvaluator(item, company);
+                item.value = evaluator.value;
             }
         });
     }
@@ -48,16 +50,55 @@ export class CompanyInfoModel {
             if (section) {
                 section.items.forEach(item => {
                     if (item.key) {
-                        item.value = this.eval(item.key, data, item.type);
+                        let evaluator = new CompanyInfoEvaluator(item, data);
+                        item.value = evaluator.value;
                     }
+                    else item.value = '-';
                 });
             }
         });
     }
 
     public setPhoneNumbersData(group: number, title: string, section: any): void {}
+}
 
-    private eval(property: string, data: any, type?: string): any {
+export class CompanyInfoSectionGroup {
+    public sections: CompanyInfoSection[];
+}
+
+export class CompanyInfoSection {
+    public title: string;
+    public cssClass: string;
+    public items: CompanyInfoItem[];
+}
+
+export class CompanyInfoItem {
+    public key: string;
+    public title: string;
+    public value: any;
+
+    public cssClass: string;
+    public type: string;
+    public format: string;
+}
+
+export class CompanyInfoEvaluator {
+    private _baseData: any;
+    private _item: CompanyInfoItem;
+    private _value: string;
+
+    get value(): string {
+        return this._value;
+    }
+
+    constructor(item: CompanyInfoItem, data: any) {
+        this._baseData = data;
+        this._item = item;
+
+        this._value = this.eval(this._item.key, this._baseData);
+    }
+    
+    private eval(property: string, data: any, applyFormatting: boolean = true): any {
         if (!data) return null;
 
         let matches = property.match(/^([^\.]+)\.(.*)$/);
@@ -75,39 +116,64 @@ export class CompanyInfoModel {
             else {
                 evalObj = data[baseProperty];
             }
-            return this.eval(restProperty, evalObj, type);
+            return this.eval(restProperty, evalObj, applyFormatting);
         }
         else {
             if (data.hasOwnProperty(property)) {
                 let value = data[property];
-                switch (type) {
-                    case 'date':
-                        let datePipe = new DatePipe("en-US");
-                        value = datePipe.transform(value, 'MMMM d, yyyy');
-                        break;
-                    // case 'phone':
+                if (applyFormatting) {
+                    value = this.evalFormat(value, this._item.type, this._item.format);
                 }
                 return value;
             }
             return null;
         }
     }
-}
+    
+    private evalFormat(value: any, type: string, format: string): any {
+        let formatPipe;
 
-export class CompanyInfoSectionGroup {
-    public sections: CompanyInfoSection[];
-}
-
-export class CompanyInfoSection {
-    public title: string;
-    public cssClass: string;
-    public items: CompanyInfoItem[];
-}
-
-export class CompanyInfoItem {
-    public key: string;
-    public title: string;
-    public cssClass: string;
-    public type: string;
-    public value: any;
+        switch (type) {
+            case 'date':
+                // --
+                // Pattern: {DatePipeFormat}
+                // --
+                formatPipe = new DatePipe('en-US');
+                format = format ? format : 'MMMM d, yyyy';
+                value = formatPipe.transform(value, format);
+                break;
+            case 'number':
+                // --
+                // Pattern: {minIntegerDigits}.{minFractionDigits}-{maxFractionDigits}
+                // --
+                formatPipe = new DecimalPipe('en-US');
+                format = format ? format : '1.0-0';
+                value = formatPipe.transform(value, format);
+                break;
+            case 'currency':
+                // --
+                // Pattern: [ symbol | ${evalExpression} ] `|` {DecimalPipeFormat}
+                // --
+                let args = format.split('|');
+                if (args.length == 1) {
+                    value = this.evalFormat(value, 'number', args[0]);
+                }
+                else {
+                    let symbol = '';
+                    let matches = args[0].match(/^\${([^}]+)}(\s*)/);
+                    if (matches && matches.length) {
+                        symbol = this.eval(matches[1], this._baseData, false);
+                    }
+                    else {
+                        symbol = args[0];
+                    }
+                    let numberValue = this.evalFormat(value, 'number', args[1]);
+                    return `${symbol}${numberValue}`;
+                }
+                break;
+            case 'phone':
+                break;
+        }
+        return value;
+    }
 }
