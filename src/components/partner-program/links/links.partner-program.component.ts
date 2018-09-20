886a3db483a99fd217424b2a18d36baca1ce52b0
s.lebedinskiy <s.lebedinskiy@encry.ru>
2018-09-21 01:06:45 +0300

    ET-1571 Delete link issue fixed

--- src/components/partner-program/links/links.partner-program.component.ts
+++ src/components/partner-program/links/links.partner-program.component.ts
@@ -4,6 +4,9 @@ import {PartnerProgramService} from '../../../services/partner-program.service';
 import {PartnerProgramItem, PartnerProgramModel} from "../../../models/partner-program.model";
 import {TableInfoExModel, TableInfoItem} from '../../../models/base.model';
 import {killEvent} from '../../../shared/shared.functions';
+import {ModalEx} from '../../../elements/pbx-modal/pbx-modal.component';
+import {Lockable, Locker} from '../../../models/locker.model';
+
 
 @Component({
     selector: 'links-partner-program-component',
@@ -12,8 +15,13 @@ import {killEvent} from '../../../shared/shared.functions';
     animations: [FadeAnimation('300ms')],
     providers: [PartnerProgramService]
 })
+export class LinksPartnerProgramComponent implements OnInit, Lockable {
+    table: TableInfoExModel;
+    selected: PartnerProgramItem;
 
-export class LinksPartnerProgramComponent implements OnInit {
+    modalDelete: ModalEx;
+    locker: Locker;
+
     @Input() partners: PartnerProgramModel;
 
     @Output() onReload: EventEmitter<any> = new EventEmitter<any>();
@@ -21,22 +29,16 @@ export class LinksPartnerProgramComponent implements OnInit {
     @Output() onEdit: EventEmitter<PartnerProgramItem> = new EventEmitter();
     @Output() onCopyToClipboard: EventEmitter<PartnerProgramItem> = new EventEmitter();
 
-    public table: TableInfoExModel = new TableInfoExModel();
+    // -- properties ----------------------------------------------------------
 
-    modalDelete = {
-        visible: false,
-        title: '',
-        body: 'Are you sure?',
-        buttons: [
-            {type: 'cancel', value: 'Cancel'},
-            {type: 'error', value: 'Delete'},
-        ]
-    };
-    loading = 0;
-    selected: PartnerProgramItem;
-    tableReload = 0;
+    get tableSortOrder(): any {
+        return this.table ? this.table.sort : null;
+    }
 
+    // -- component lifecycle methods -----------------------------------------
+    
     constructor(private _service: PartnerProgramService) {
+        this.table = new TableInfoExModel();
         this.table.sort.isDown = false;
         this.table.sort.column = 'name';
         this.table.items = [
@@ -47,6 +49,17 @@ export class LinksPartnerProgramComponent implements OnInit {
             new TableInfoItem('Registered', 'registered', 'registered'),
             new TableInfoItem('Rewards Summary', 'totalBonus', 'totalBonus'),
         ];
+        
+        this.modalDelete = new ModalEx('Are you sure?', 'delete');
+        this.locker = new Locker();
+    }
+
+    ngOnInit(): void {}
+
+    // -- event handlers ------------------------------------------------------
+
+    reload(item) {
+        this.onReload.emit(item);
     }
 
     changePage(page: number) {
@@ -60,10 +73,6 @@ export class LinksPartnerProgramComponent implements OnInit {
         this.onReload.emit();
     }
 
-    reload(item) {
-        this.onReload.emit(item);
-    }
-
     select(item: PartnerProgramItem) {
         this.onSelect.emit(item);
     }
@@ -73,19 +82,10 @@ export class LinksPartnerProgramComponent implements OnInit {
     }
 
     delete(item: PartnerProgramItem) {
+        console.log('delete');
         this.selected = item;
         this.modalDelete.title = item.name;
-        this.modalDelete.visible = true;
-    }
-
-    doDeleteLink() {
-        this.selected.loading++;
-        this._service.deleteById(this.selected.id).then(res => {
-            this.reload(this.selected);
-            this.selected.loading--;
-        }).catch(() => {
-            this.selected.loading--;
-        });
+        this.modalDelete.show();
     }
 
     clickCreateLink() {
@@ -93,21 +93,26 @@ export class LinksPartnerProgramComponent implements OnInit {
         this.onEdit.emit(item);
     }
 
-    sort() {
-        this.tableReload++;
-        this._service.getItems(this.partners, null, this.table ? this.table.sort : null).then(res => {
-            this.partners = res;
-            this.tableReload--;
-        }).catch(() => {
-            this.tableReload--;
-        });
-    }
-
     copyToClipboard(item: PartnerProgramItem, event: MouseEvent): void {
         this.onCopyToClipboard.emit(item);
         killEvent(event);
     }
 
-    ngOnInit(): void {
+    // -- data processing methods ---------------------------------------------
+
+    doDeleteLink() {
+        this.selected.loading ++;
+        this._service.deleteById(this.selected.id).then(() => {
+            this.reload(this.selected);
+        }).catch(() => {})
+          .then(() => this.selected.loading --);
+    }
+
+    sort() {
+        this.locker.lock();
+        this._service.getItems(this.partners, null, this.tableSortOrder).then(response => {
+            this.partners = response;
+        }).catch(() => {})
+          .then(() => this.locker.unlock());
     }
 }
