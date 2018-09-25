81f9ca5da29812f605e7ea5ec412728ea965b98c
p.kamynin <codemusic@mail.ru>
2018-09-25 15:20:17 +0300

    chat - talking

--- src/assets/js/stuffs.js
+++ src/assets/js/stuffs.js
@@ -9,4 +9,22 @@ jQuery(document).ready(function(){
         })
     }, 500);
 
+    // автоматическая прокрутка чата вниз при нажатии на 'chat'
+    function scrollToDown(){
+        var scrollToDown = $('pbx-chat .scrollToDown');
+        scrollToDown.scrollTop(scrollToDown.prop('scrollHeight'));
+    }
+    setInterval(function(){
+        $('.header_button.chat').on('click', function(){
+            scrollToDown();
+        });
+        $('pbx-chat input').keyup(function(e){
+            if(e.keyCode == 13){
+                setTimeout(function(){
+                    scrollToDown();
+                }, 500);
+            }
+        });
+    }, 500);
+
 });
