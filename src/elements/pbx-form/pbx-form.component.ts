ce86840a4099dfe66c0007e0b3efb0ffba0d6f33
s.kozulyaev <s.kozulyaev@encry.ru>
2018-10-16 17:27:21 +0300

    task ET-1490

--- src/elements/pbx-form/pbx-form.component.ts
+++ src/elements/pbx-form/pbx-form.component.ts
@@ -18,7 +18,7 @@ export class FormComponent implements OnInit {
     @Input() confirm: { value: string, buttonType: string, inactive: boolean, loading: boolean };
     @Input() decline: { value: string, buttonType: string, inactive: boolean, loading: boolean };
 
-    @Input() background: boolean;
+    @Input() background: string;
     @Input() fillBackground: boolean;
 
     @Output() onConfirm: EventEmitter<void> = new EventEmitter<void>();
