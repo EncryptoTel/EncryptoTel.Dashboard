f6bf476623db015813a160c51e93750d007e95c5
Sergey Pashnin <s.pashnin@encry.ru>
2018-10-31 16:32:34 +0300

    правил кÐпозиционирование элÐ + в еуÑ+btn resetÐtemplet- Ð #25

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,17 @@
+// export const environment = {
+//     title: 'development',
+//     production: false,
+//     backApiVersion: 'v1',
+//     translation: 'http://pbx-front-dev.encry.ru/translation/translations',
+//     back: 'http://pbx-front-dev.encry.ru/api',
+//     ws: 'ws.pbx-front-dev.encry.ru',
+// };
+
 export const environment = {
-    title: 'development',
+    title: 'test',
     production: false,
     backApiVersion: 'v1',
-    translation: 'http://pbx-front-dev.encry.ru/translation/translations',
-    back: 'http://pbx-front-dev.encry.ru/api',
-    ws: 'ws.pbx-front-dev.encry.ru',
+    translation: 'http://pbx-back-test.encry.ru/translation/translations',
+    back: 'http://pbx-back-test.encry.ru/front-api',
+    ws: 'ws.pbx-front-test.encry.ru',
 };
