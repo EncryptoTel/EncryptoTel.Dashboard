feb5bd37bd681a5ab68fcdeb32ff4b1d6e2df676
s.kozulyaev <s.kozulyaev@encry.ru>
2018-11-14 14:28:44 +0300

    upload button click on small width

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,16 @@ export const environment = {
     title: 'development',
     production: false,
     backApiVersion: 'v1',
-    translation: '/translations',
-    back: '/front-api',
+    translation: 'http://pbx-front-dev.encry.ru/translation/translations',
+    back: 'http://pbx-front-dev.encry.ru/front-api',
     ws: 'ws.pbx-front-dev.encry.ru',
 };
+
+// export const environment = {
+//     title: 'development',
+//     production: false,
+//     backApiVersion: 'v1',
+//     translation: 'http://back-symfony.loc/translation/translations',
+//     back: 'http://back-symfony.loc/front-api',
+//     ws: 'ws.back-symfony.loc',
+// };
