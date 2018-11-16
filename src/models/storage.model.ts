324b4aaa5969515f370c74d014478fd9778f27d3
s.kozulyaev <s.kozulyaev@encry.ru>
2018-11-16 21:09:54 +0300

    task ET-1750 Storage

--- src/models/storage.model.ts
+++ src/models/storage.model.ts
@@ -9,7 +9,6 @@ export class StorageModel extends PageInfoModel {
 export class StorageItem extends BaseItemModel {
     public accountId: number;
     @Type(() => Date)
-    public id: number;
     public created: Date;
     public description: string;
     public duration: number;
