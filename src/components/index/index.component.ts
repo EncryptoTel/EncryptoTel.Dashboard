6141eeee3f8586ac59ea09a472e1ffaa8c48be47
p.kamynin <codemusic@mail.ru>
2018-12-12 14:02:15 +0300

    Merge branch 'test' of C:\Users\codem\Desktop\first\FrontDesktop with conflicts.

--- src/components/index/index.component.ts
+++ src/components/index/index.component.ts
@@ -55,7 +55,7 @@ export class IndexComponent implements OnInit, OnDestroy {
     userNavigationVisible: boolean = false;
     mobileNavigationVisible: boolean = false;
     NotificationSubscription: Subscription;
-    isLockedCaller: boolean = true;
+    isLockedCaller: boolean = false;
     get username(): string {
         if (this.user && this.user.profile) {
             let firstName: string;
