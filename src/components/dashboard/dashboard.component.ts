2e2b6b4d14ad9867fd3343e179a7e88df4fe073e
s.lebedinskiy <s.lebedinskiy@encry.ru>
2019-01-23 23:49:13 +0300

    http://redmine.encry.ru/issues/2200 WIP

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -1,77 +1,77 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
-import {CallDetailItem, CallDetailModel, DashboardModel} from '../../models/dashboard.model';
-import {DashboardServices} from '../../services/dashboard.services';
+import { CallDetailItem, CallDetailModel, DashboardModel } from '@models/dashboard.model';
+import { DashboardServices } from '@services/dashboard.services';
+import { WsServices } from '@services/ws.services';
+import { LocalStorageServices } from '@services/local-storage.services';
 
-import {Subscription} from 'rxjs/Subscription';
-import {WsServices} from '../../services/ws.services';
-import {LocalStorageServices} from '@services/local-storage.services';
 
 @Component({
-    selector: 'pbx-dashboard',
-    templateUrl: './template.html',
-    styleUrls: ['./local.sass'],
-    providers: [
-        DashboardServices,
-    ]
+  selector: 'pbx-dashboard',
+  templateUrl: './template.html',
+  styleUrls: ['./local.sass'],
+  providers: [
+    DashboardServices,
+  ]
 })
-
 export class DashboardComponent {
 
-    user: any;
-    dateFormat: any;
+  user: any;
+  dateFormat: any;
 
-    constructor(private _dashboard: DashboardServices,
-                private _ws: WsServices,
-                private storage: LocalStorageServices) {
-        this.initDashboard();
-        this.user = this.storage.readItem('pbx_user');
-        this.dateFormat = this.storage.readItem('dateFormat');
+  dashboard: DashboardModel;
+  loading = true;
+  balanceSubscription: Subscription;
+  cdrSubscription: Subscription;
+
+  constructor(private _dashboard: DashboardServices,
+    private _ws: WsServices,
+    private storage: LocalStorageServices) {
+    this.initDashboard();
+    this.user = this.storage.readItem('pbx_user');
+    this.dateFormat = this.storage.readItem('dateFormat');
+  }
+
+  fetchDashboard(item = null) {
+    (item ? item : this).loading = true;
+    this._dashboard.getDashboard()
+      .then(dashboard => {
+        this.dashboard = dashboard;
+        (item ? item : this).loading = false;
+      }).catch((error) => {
+        console.log('catch fetchDashboard', error);
+        (item ? item : this).loading = false;
+      });
+  }
+
+  initDashboard(): void {
+    this.fetchDashboard();
+
+    this.balanceSubscription = this._ws.getBalance().subscribe(balance => {
+      this.dashboard.balance.value = balance.balance;
+    });
+    this.cdrSubscription = this._ws.subCdr().subscribe(() => {
+      let model = null;
+      if (this.dashboard.callDetail.length > 0) {
+        model = this.dashboard.callDetail[0];
+      }
+      if (!model) {
+        model = new CallDetailModel();
+        model.date = new Date();
+        this.dashboard.callDetail.unshift(model);
+      }
+      const item = new CallDetailItem();
+      model.list.unshift(item);
+      this.fetchDashboard(item);
+    });
+  }
+
+  isAdmin() {
+    if (!this.user.profile.tariffPlan) {
+      return false;
+    } else {
+      return true;
     }
-
-    dashboard: DashboardModel;
-    loading = true;
-    balanceSubscription: Subscription;
-    cdrSubscription: Subscription;
-
-    fetchDashboard(item = null) {
-        (item ? item : this).loading = true;
-        this._dashboard.getDashboard().then(dashboard => {
-            this.dashboard = dashboard;
-            (item ? item : this).loading = false;
-        }).catch((error) => {
-            console.log('catch fetchDashboard', error);
-            (item ? item : this).loading = false;
-        });
-    }
-
-    initDashboard(): void {
-        this.fetchDashboard();
-
-        this.balanceSubscription = this._ws.getBalance().subscribe(balance => {
-            this.dashboard.balance.value = balance.balance;
-        });
-        this.cdrSubscription = this._ws.subCdr().subscribe(() => {
-            let model = null;
-            if (this.dashboard.callDetail.length > 0) {
-                model = this.dashboard.callDetail[0];
-            }
-            if (!model) {
-                model = new CallDetailModel();
-                model.date = new Date();
-                this.dashboard.callDetail.unshift(model);
-            }
-            const item = new CallDetailItem();
-            model.list.unshift(item);
-            this.fetchDashboard(item);
-        });
-    }
-
-    isAdmin() {
-        if (!this.user.profile.tariffPlan) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+  }
 }
