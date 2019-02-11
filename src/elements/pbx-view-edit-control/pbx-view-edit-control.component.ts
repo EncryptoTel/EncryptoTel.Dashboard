d56c5892ed7a9fb7dfa14e5cf0895c046b9bef4a
s.lebedinskiy <s.lebedinskiy@encry.ru>
2019-02-12 01:35:25 +0300

    departments - odd checkboxes removed

--- src/elements/pbx-view-edit-control/pbx-view-edit-control.component.ts
+++ src/elements/pbx-view-edit-control/pbx-view-edit-control.component.ts
@@ -6,99 +6,111 @@ import { ModalEx } from '@elements/pbx-modal/pbx-modal.component';
 import { MessageServices } from '@services/message.services';
 
 
+export interface ITableInfo {
+  keys: string[];
+  titles: string[];
+}
+
 @Component({
-    selector: 'pbx-view-edit-control',
-    templateUrl: './template.html',
-    styleUrls: ['./local.sass'],
-    animations: [FadeAnimation('300ms')]
+  selector: 'pbx-view-edit-control',
+  templateUrl: './template.html',
+  styleUrls: ['./local.sass'],
+  animations: [FadeAnimation('300ms')]
 })
 export class ViewEditControlComponent implements OnInit {
 
-    editMode: boolean;
-    modal: ModalEx;
-    addMembersCount: number = 0;
+  editMode: boolean;
+  modal: ModalEx;
+  addMembersCount: number = 0;
+  tableInfoSelected: ITableInfo;
 
-    @Input() headerText: string;
-    @Input() buttonText: string;
-    @Input() noDataMessage: string = 'Nothing found';
-    @Input() parentEditMode: boolean = true;
+  @Input() headerText: string;
+  @Input() buttonText: string;
+  @Input() noDataMessage: string = 'Nothing found';
+  @Input() parentEditMode: boolean = true;
 
-    @Input() tableInfo: {};
-    @Input() items: any[];
-    @Input() selectedItems: any[];
+  @Input() tableInfo: ITableInfo;
+  @Input() items: any[];
+  @Input() selectedItems: any[];
 
-    @Output() onEditModeChanged: EventEmitter<boolean>;
+  @Output() onEditModeChanged: EventEmitter<boolean>;
 
-    nothingFoundText: string;
+  nothingFoundText: string;
 
-    get hasData(): boolean {
-        return !!this.selectedItems && this.selectedItems.length > 0;
+  get hasData(): boolean {
+    return !!this.selectedItems && this.selectedItems.length > 0;
+  }
+
+  get allSelected(): boolean {
+    return this.items.length === this.selectedItems.length;
+  }
+
+  constructor(
+    public translate: TranslateService,
+    public messages: MessageServices
+  ) {
+    this.editMode = false;
+    this.onEditModeChanged = new EventEmitter();
+    this.addMembersCount = 0;
+
+    this.nothingFoundText = this.translate.instant('Nothing found');
+    this.modal = new ModalEx('', 'delete');
+    this.modal.body = this.translate.instant('Are you sure you want to delete this member?');
+  }
+
+  ngOnInit(): void {
+    if (this.tableInfo) {
+      this.tableInfoSelected = {
+        keys: this.tableInfo.keys.slice(1),
+        titles: this.tableInfo.titles.slice(1)
+      };
     }
-    
-    get allSelected(): boolean {
-      return this.items.length === this.selectedItems.length;
-    }
+  }
 
-    constructor(
-        private renderer: Renderer2, 
-        public translate: TranslateService,
-        public messages: MessageServices
-    ) {
-        this.editMode = false;
-        this.onEditModeChanged = new EventEmitter();
-        this.addMembersCount = 0;
-
-        this.nothingFoundText = this.translate.instant('Nothing found');
-        this.modal = new ModalEx('', 'delete');
-        this.modal.body = this.translate.instant('Are you sure you want to delete this member?');
-    }
-
-    ngOnInit(): void { }
-
-    toggleEditMode(): void {
-        if (this.editMode && this.addMembersCount > 0) {
-            const confirmation: string = this.translate
-                // .instant('departmentMembersAddedMessage', { count: this.addMembersCount });
-                .instant('departmentMembersAddedMessage', { count: this.selectedItems.length });
-            this.messages.writeSuccess(confirmation);
-            this.addMembersCount = 0;
-        }
-
-        this.editMode = !this.editMode;
-        this.onEditModeChanged.emit(this.editMode);
-    }
-
-    selectItem(item: any): void {
-        if (!this.selectedItems) this.selectedItems = [];
-        if (this.isSelected(item)) {
-            this.deleteSelectedItem(item);
-        }
-        else {
-            this.selectedItems.push(item);
-            this.addMembersCount ++;
-        }
+  toggleEditMode(): void {
+    if (this.editMode && this.addMembersCount > 0) {
+      const confirmation: string = this.translate
+        // .instant('departmentMembersAddedMessage', { count: this.addMembersCount });
+        .instant('departmentMembersAddedMessage', { count: this.selectedItems.length });
+      this.messages.writeSuccess(confirmation);
+      this.addMembersCount = 0;
     }
 
-    toggleAll(selectAll: boolean): void {
-      this.selectedItems = [];
-      if (selectAll) {
-        this.items
-          .forEach(item => this.selectedItems.push(item));
-      }
-    }
+    this.editMode = !this.editMode;
+    this.onEditModeChanged.emit(this.editMode);
+  }
 
-    isSelected(item: any): boolean {
-        return this.selectedItems.find(i => i.id === item.id) ? true : false;
+  selectItem(item: any): void {
+    if (!this.selectedItems) this.selectedItems = [];
+    if (this.isSelected(item)) {
+      this.deleteSelectedItem(item);
     }
+    else {
+      this.selectedItems.push(item);
+      this.addMembersCount++;
+    }
+  }
 
-    delete(item: any): void {
-        this.modal.confirmCallback = () => {
-            this.deleteSelectedItem(item);
-        };
-        this.modal.show();
+  toggleAll(selectAll: boolean): void {
+    this.selectedItems = [];
+    if (selectAll) {
+      this.items
+        .forEach(item => this.selectedItems.push(item));
     }
+  }
 
-    deleteSelectedItem(item: any): void {
-        this.selectedItems = this.selectedItems.filter(i => i.id !== item.id);
-    }
+  isSelected(item: any): boolean {
+    return this.selectedItems.find(i => i.id === item.id) ? true : false;
+  }
+
+  delete(item: any): void {
+    this.modal.confirmCallback = () => {
+      this.deleteSelectedItem(item);
+    };
+    this.modal.show();
+  }
+
+  deleteSelectedItem(item: any): void {
+    this.selectedItems = this.selectedItems.filter(i => i.id !== item.id);
+  }
 }
