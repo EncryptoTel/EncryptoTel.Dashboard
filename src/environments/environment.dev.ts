97471d4bbbf63bccc5721fab5a2573ecc95592fc
s.kozulyaev <s.kozulyaev@encry.ru>
2019-02-14 15:07:13 +0300

    fix translation

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
     title: 'development',
     production: false,
     backApiVersion: 'v1',
-    translation: 'http://pbx-front-dev.encry.ru/translation/translations',
+    translation: 'https://pbx.encryptotel.com/translation/translations',
     back: 'http://pbx-front-dev.encry.ru/front-api',
     ws: 'ws.pbx-front-dev.encry.ru',
     version: '1.0.199'
