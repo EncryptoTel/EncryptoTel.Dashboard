a2b65df23980f2b5f835809e8c9780b8ecf1c813
s.kozulyaev <s.kozulyaev@encry.ru>
2019-02-19 10:51:55 +0300

    task http://redmine.encry.ru/issues/2464

---
+++ src/environments/environment.ett24.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    title: 'production',
+    production: false,
+    backApiVersion: 'v1',
+    translation: 'https://pbx.encryptotel.com/translation/translations',
+    back: 'https://back-ett.encry.ru/front-api',
+    ws: 'ws.back-ett.encry.ru',
+    version: '1.0.199'
+};
