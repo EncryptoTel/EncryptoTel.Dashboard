4f7cf9610b72965372774f2d29e57fe385f7662a
s.kozulyaev <s.kozulyaev@encry.ru>
2019-02-19 12:24:12 +0300

    fix env

--- src/environments/environment.ett24.ts
+++ src/environments/environment.ett24.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: false,
     backApiVersion: 'v1',
     translation: 'https://pbx.encryptotel.com/translation/translations',
-    back: 'https://ett.encry.ru/front-api',
+    back: 'http://ett.encry.ru/front-api',
     ws: 'ws.ett.encry.ru',
     version: '1.0.199'
 };
