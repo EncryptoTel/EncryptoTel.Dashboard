acc1db477d321dd3565f34b00536af2f1b78aae4
s.kozulyaev <s.kozulyaev@encry.ru>
2019-02-26 18:09:42 +0300

    fix callRule sidebar

--- src/components/call-rules/call-rules.component.ts
+++ src/components/call-rules/call-rules.component.ts
@@ -1,11 +1,12 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, OnInit} from '@angular/core';
 import {FadeAnimation} from '../../shared/fade-animation';
 import {CallRulesService} from '../../services/call-rules.service';
-import {CallRulesModel} from '../../models/call-rules.model';
+import {Action, CallRulesItem, CallRulesModel} from '../../models/call-rules.model';
 import {TranslateService} from '@ngx-translate/core';
-import { TableInfoExModel, TableInfoItem } from '@models/base.model';
+import {SidebarButtonItem, SidebarInfoItem, SidebarInfoModel, TableInfoExModel, TableInfoItem} from '@models/base.model';
 import {ListComponent} from '@elements/pbx-list/pbx-list.component';
 import { MessageServices } from '@services/message.services';
+import {AddressBookItem} from '@models/address-book.model';
 
 
 @Component({
@@ -15,17 +16,22 @@ import { MessageServices } from '@services/message.services';
     animations: [FadeAnimation('300ms')]
 })
 
-export class CallRulesComponent {
+export class CallRulesComponent implements OnInit{
 
     table: TableInfoExModel = new TableInfoExModel();
     pageInfo: CallRulesModel = new CallRulesModel();
     @ViewChild(ListComponent) list: ListComponent;
+
+    sidebar: CallRulesItem = null;
+    selected: CallRulesItem;
+    actionsList: any;
 
     constructor(
         public service: CallRulesService,
         public translate: TranslateService,
         public message: MessageServices
     ) {
+
         this.table.sort.isDown = true;
         this.table.sort.column = 'phoneNumber';
         this.table.items.push(new TableInfoItem(this.translate.instant('Phone number'), 'phoneNumber', 'phoneNumber'));
@@ -45,5 +51,89 @@ export class CallRulesComponent {
     onDelete(): void {
         const delMessage: string = this.translate.instant('Call Rule has been deleted successfully');
         this.message.writeSuccess(delMessage);
+    }
+
+    select(item) {
+        console.log(item);
+        this.selected = item;
+        this.sidebar = this.sidebar ? (this.sidebar.id === item.id ? null : item) : item;
+    }
+
+    private getParams(): void {
+        this.service.getParams().then(response => {
+            this.actionsList = [];
+            response.actions.forEach(item => {
+                this.actionsList[item.id] = this.translate.instant(item.code);
+            });
+        })
+        .catch(() => {})
+        .then(() => {});
+    }
+
+    getActionName(action) {
+        return this.actionsList[action];
+    }
+
+    getActionParamsName(action) {
+        let result: any;
+        switch (action) {
+            case 1:
+                result = this.translate.instant('Extension number');
+                break;
+            case 2:
+                result = this.translate.instant('Extension number');
+                break;
+            case 3:
+                result = this.translate.instant('Queue');
+                break;
+            case 4:
+                result = '';
+                break;
+            case 5:
+                result = this.translate.instant('Voice greeting');
+                break;
+            case 6:
+                result = this.translate.instant('Group');
+                break;
+        }
+        return result;
+    }
+
+    getActionParamsValue(item) {
+        let result: any;
+        switch (item.action) {
+            case 1:
+                result = item.sipInner.phoneNumber;
+                break;
+            case 2:
+                result = this.translate.instant('Extension number');
+                break;
+            case 3:
+                result = item.callQueue.name;
+                break;
+            case 4:
+                result = '';
+                break;
+            case 5:
+                result = item.accountFile.fileName;
+                break;
+            case 6:
+                result = item.ringGroup.name;
+                break;
+        }
+        return result;
+    }
+
+    ngOnInit() {
+        this.getParams();
+    }
+
+    edit() {
+
+    }
+
+    cancel() {
+        this.sidebar = null;
+        this.selected = null;
     }
 }
